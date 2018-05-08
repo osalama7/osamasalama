dc3dd639d37b833e60fb0d572257ea83af436e55
Osama Salama <osama.m7@gmail.com>
2018-05-08 17:32:54 +0200

    starting on web profile

---
+++ README.md
@@ -0,0 +1,1 @@
+**Osama Salama Profile**
---
+++ gatsby-config.js
@@ -0,0 +1,15 @@
+const contentfulConfig = require('./.contentful')
+
+module.exports = {
+  pathPrefix: '/gatsby-contentful-starter',
+  plugins: [
+    'gatsby-transformer-remark',
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-source-contentful',
+      options: process.env.NODE_ENV === 'development' ?
+        contentfulConfig.development :
+        contentfulConfig.production
+    },
+  ],
+}
